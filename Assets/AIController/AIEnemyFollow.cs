dd12ea1e0166e866201cc9c9441ea862ae142112
jackliu1001 <liujack1001@yahoo.com>
2022-01-13 11:55:26 -0800

    misc.
    tilesets, state machine, new scene

--- Assets/AIController/AIEnemyFollow.cs
+++ Assets/AIController/AIEnemyFollow.cs
@@ -9,7 +9,7 @@ public class AIEnemyFollow : StateMachineBehaviour
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         enemyMovement = animator.gameObject.GetComponent<EnemyMovement>();
-        enemyMovement.MovementState = EnemyMovement.MovementStates.follow;
+        enemyMovement.followStart();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
